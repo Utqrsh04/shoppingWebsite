cf4e84fb744ea36a7c907302b7738bb460d6928c
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-08-29 11:44:08 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, rem!
+        Doloremque dolore quos, quis necessitatibus consectetur iusto non ea!
+        Beatae dolor quidem eligendi quos maxime dolorem omnis voluptate velit
+        corrupti error quia impedit reprehenderit natus fugiat ullam soluta,
+        eius illum fuga voluptas autem excepturi unde dignissimos. Voluptas
+        inventore esse assumenda.
       </header>
     </div>
   );
