9c57f516917c4bccf68b4ae8b42bf28d20a79375
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-08-29 21:56:52 +0530

    added product card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Hero from "./components/HeroSection/Hero";
+import Loader from "./components/Loader/Loader";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -15,38 +15,11 @@ function App() {
   }, []);
 
   return loading ? (
-    <div className="loader-container">
-      <span class="dot"></span>
-      <div class="dots">
-        <span className="loader_dots"></span>
-        <span className="loader_dots"></span>
-        <span className="loader_dots"></span>
-      </div>
-    </div>
+    <Loader />
   ) : (
     <div className="App">
       <div>
-        <Header />
-        <Hero />
-        <header className="App-header">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, rem!
-          Doloremque dolore quos, quis necessitatibus consectetur iusto non ea!
-          Beatae dolor quidem eligendi quos maxime dolorem omnis voluptate velit
-          corrupti error quia impedit reprehenderit natus fugiat ullam soluta,
-          eius illum fuga voluptas autem excepturi unde dignissimos. Voluptas
-          inventore esse assumenda. Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. Quae consequuntur expedita facere culpa. Nam
-          repudiandae modi totam. Pariatur ipsa consectetur tenetur iusto vel
-          obcaecati, possimus non velit quod molestias. Laborum natus eaque illo
-          sit commodi odit eum omnis quo? Eius soluta non consequatur modi quos,
-          accusamus tenetur numquam cumque laborum molestiae magnam amet
-          dignissimos aliquid unde corrupti dolore blanditiis? Aliquam nesciunt
-          quos consequuntur, odio excepturi fugiat quod sed aperiam, enim esse
-          cupiditate voluptates. Eum, sint exercitationem debitis eaque
-          voluptatem eos placeat vel aperiam voluptatum ratione saepe labore
-          delectus ipsum nesciunt rerum nihil illum nostrum vitae quae assumenda
-          corporis tempora! Neque.
-        </header>
+        <LandingPage />
       </div>
     </div>
   );
---
+++ src/components/ProductCard.jsx/ProductCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ProductCard.scss";
+
+const ProductCard = ({ img }) => {
+  return (
+    <div className="product_wrapper">
+      <div className="product_image_container">
+        <img src={img} alt="productImage" />
+      </div>
+
+      <div className="product_details_wrapper">
+        <p>The Cross-Back Apron Mini Dress</p>
+        <p>₹ 7220</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
