572991599ffe4aa9f0fe5340720754bfcd8aff4c
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-08-30 11:00:09 +0530

    added react-router-dom and setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 import Loader from "./components/Loader/Loader";
 import LandingPage from "./pages/LandingPage/LandingPage";
+import Contact from "./pages/ContactPage/Contact";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import ProductDescPage from "./pages/ProductDescPage/ProductDescPage";
+import LoginForm from "./components/LoginForm/LoginForm";
+import SignupForm from "./components/SignupForm/SignupForm";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -14,14 +20,30 @@ function App() {
     }, 2000);
   }, []);
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <div className="App">
-      <div>
-        <LandingPage />
-      </div>
-    </div>
+  return (
+    <>
+      <BrowserRouter>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/signup" element={<SignupForm />}></Route>
+              <Route path="/login" element={<LoginForm />}></Route>
+              <Route path="/" element={<LandingPage />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/products" element={<ProductsPage />}></Route>
+
+              <Route
+                path="product/:productId"
+                element={<ProductDescPage />}
+              ></Route>
+            </Routes>
+          </>
+        )}
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
 import Hero from "../../components/HeroSection/Hero";
 import NewArrivals from "../../components/NewArrivals/NewArrivals";
 
 function LandingPage() {
   return (
     <>
-      <Header />
       <Hero />
       <NewArrivals />
       <Footer />
